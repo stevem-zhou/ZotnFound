6e14b85727007fa8cdd4f6b00b3ff03c1b7a1312
NwinNwin <ndnguyen2003@gmail.com>
2023-05-27 01:49:20 -0700

    added test data and fixed minor bug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './components/Login/Login'
-import Home from './components/Home/Home'
-import ResultCard from './components/ResultCard/ResultCard'
-import CreateModal from './components/CreateModal/CreateModal'
-import { ChakraProvider } from '@chakra-ui/react'
-import 'leaflet/dist/leaflet.css'
-
-import { ChakraProvider } from '@chakra-ui/react'
-
-function App () {
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import ResultCard from "./components/ResultCard/ResultCard";
+import CreateModal from "./components/CreateModal/CreateModal";
+import { ChakraProvider } from "@chakra-ui/react";
+import "leaflet/dist/leaflet.css";
+function App() {
   return (
     <ChakraProvider>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/cards' element={<ResultCard />} />
-          ;<Route path='/modals' element={<CreateModal />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cards" element={<ResultCard />} />
+          ;<Route path="/modals" element={<CreateModal />} />
         </Routes>
       </div>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
