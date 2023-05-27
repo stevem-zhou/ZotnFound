814f87d51a2f2e647b1f38f3fad467798f3da9ec
NwinNwin <ndnguyen2003@gmail.com>
2023-05-27 15:14:19 -0700

    added padding for logout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Map from '../Map/Map'
-import './Home.css'
-import Filter from '../Filter/Filter'
-import ResultsBar from '../ResultsBar/ResultsBar'
-import { auth } from '../../firebase'
-import { signOut } from 'firebase/auth'
+import React from "react";
+import Map from "../Map/Map";
+import "./Home.css";
+import Filter from "../Filter/Filter";
+import ResultsBar from "../ResultsBar/ResultsBar";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 // import { Search2Icon } from "@chakra-ui/icons";
-import { AuthContext } from '../../context/AuthContext'
-import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-import fakeData from '../../fakeData'
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import fakeData from "../../fakeData";
 import CreateModal from "../CreateModal/CreateModal";
 import {
   Input,
@@ -18,54 +18,54 @@ import {
   Button,
   Flex,
   HStack,
-  Text
-} from '@chakra-ui/react'
-export default function Home () {
-  const { dispatch } = useContext(AuthContext)
-  const navigate = useNavigate()
+  Text,
+} from "@chakra-ui/react";
+export default function Home() {
+  const { dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const currentUser = JSON.parse(localStorage.getItem('user'))
+  const currentUser = JSON.parse(localStorage.getItem("user"));
 
-  const handleLogout = e => {
-    e.preventDefault()
+  const handleLogout = (e) => {
+    e.preventDefault();
     signOut(auth)
       .then(() => {
         // // Sign-out successful.
-        dispatch({ type: 'LOGOUT' })
-        navigate('/')
+        dispatch({ type: "LOGOUT" });
+        navigate("/");
       })
-      .catch(error => {
+      .catch((error) => {
         // An error happened.
-      })
-  }
+      });
+  };
   return (
     <div>
-      <Flex justifyContent='space-between' shadow='md'>
-        <InputGroup width='40%' ml='2%' mt='1%' size='lg' mb='1%'>
-          <InputLeftAddon children='🔎' />
-          <Input type='teal' placeholder='Search Items ...' />
+      <Flex justifyContent="space-between" shadow="md">
+        <InputGroup width="40%" ml="2%" mt="1%" size="lg" mb="1%">
+          <InputLeftAddon children="🔎" />
+          <Input type="teal" placeholder="Search Items ..." />
         </InputGroup>
-        <HStack>
-          <Text fontSize='xl' fontWeight='500' mr='4%'>
+        <HStack mr="1%">
+          <Text fontSize="xl" fontWeight="500" mr="4%">
             {currentUser?.email}
           </Text>
           <Button
-            colorScheme='teal'
-            size='lg'
-            mt='2%'
-            mr='2%'
+            colorScheme="blue"
+            size="lg"
+            mt="2%"
+            mr="5%"
             onClick={handleLogout}
           >
             Logout
           </Button>
         </HStack>
       </Flex>
-      <div className='home'>
+      <div className="home">
         <Filter />
         <Map data={fakeData} />
         <ResultsBar data={fakeData} />
         <CreateModal />
       </div>
     </div>
-  )
+  );
 }
