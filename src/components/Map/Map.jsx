f0e52b4c2e9e49f5bc683f16f3e51713f28ada2d
NwinNwin <ndnguyen2003@gmail.com>
2023-05-27 13:42:09 -0700

    worked mapping ddata to map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,113 @@
 import React from "react";
 import "./Map.css";
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import headphone_red from "../../assets/logos/headphone_red.svg";
+import headphone_green from "../../assets/logos/headphone_green.svg";
 
-export default function Map() {
+import phone_green from "../../assets/logos/phone_green.svg";
+import phone_red from "../../assets/logos/phone_red.svg";
+
+import wallet_green from "../../assets/logos/wallet_green.svg";
+import wallet_red from "../../assets/logos/wallet_red.svg";
+
+import key_green from "../../assets/logos/key_green.svg";
+import key_red from "../../assets/logos/key_red.svg";
+
+import others_red from "../../assets/logos/others_red.svg";
+import others_green from "../../assets/logos/others_green.svg";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Map({ data }) {
+  const headphoneLost = L.icon({
+    iconUrl: headphone_red,
+    iconSize: [50, 50],
+  });
+
+  const headphoneFound = L.icon({
+    iconUrl: headphone_green,
+    iconSize: [50, 50],
+  });
+
+  const phoneLost = L.icon({
+    iconUrl: phone_red,
+    iconSize: [50, 50],
+  });
+
+  const phoneFound = L.icon({
+    iconUrl: phone_green,
+    iconSize: [50, 50],
+  });
+
+  const keyLost = L.icon({
+    iconUrl: key_red,
+    iconSize: [50, 50],
+  });
+
+  const keyFound = L.icon({
+    iconUrl: key_green,
+    iconSize: [50, 50],
+  });
+
+  const walletLost = L.icon({
+    iconUrl: wallet_red,
+    iconSize: [50, 50],
+  });
+
+  const walletFound = L.icon({
+    iconUrl: wallet_green,
+    iconSize: [50, 50],
+  });
+
+  const othersLost = L.icon({
+    iconUrl: others_red,
+    iconSize: [50, 50],
+  });
+
+  const othersFound = L.icon({
+    iconUrl: others_green,
+    iconSize: [50, 50],
+  });
+
   function hi() {
     console.log("hi");
   }
+
+  const allMarkers = data.map((item) => (
+    <Marker
+      position={item.location}
+      eventHandlers={{ click: hi }}
+      icon={
+        item.type == "headphone" && item.isLost
+          ? headphoneLost
+          : item.type == "phone" && item.isLost
+          ? phoneLost
+          : item.type == "wallet" && item.isLost
+          ? walletLost
+          : item.type == "key" && item.isLost
+          ? keyLost
+          : item.type == "other" && item.isLost
+          ? othersLost
+          : item.type == "headphone" && !item.isLost
+          ? headphoneFound
+          : item.type == "phone" && !item.isLost
+          ? phoneFound
+          : item.type == "wallet" && !item.isLost
+          ? walletFound
+          : item.type == "key" && !item.isLost
+          ? keyFound
+          : item.type == "other" && !item.isLost
+          ? othersFound
+          : othersFound
+      }
+    >
+      <Popup>
+        A pretty popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  ));
+
   return (
     <div>
       <MapContainer
@@ -24,11 +125,7 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[33.6461, -117.8427]} eventHandlers={{ click: hi }}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {allMarkers}
       </MapContainer>
     </div>
   );
