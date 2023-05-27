e9fd87bd7f2d872a58cafe4167a10c3a8f351c56
Katy Huang <katyhuang360166@gmail.com>
2023-05-27 15:36:11 -0700

    search on map

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,18 +27,17 @@ export default function Home () {
   const { dispatch } = useContext(AuthContext)
   const navigate = useNavigate()
 
+  const currentUser = JSON.parse(localStorage.getItem('user'))
 
-  const currentUser = JSON.parse(localStorage.getItem("user"));
-
-  const handleLogout = (e) => {
-    e.preventDefault();
+  const handleLogout = e => {
+    e.preventDefault()
     signOut(auth)
       .then(() => {
         // // Sign-out successful.
-        dispatch({ type: "LOGOUT" });
-        navigate("/");
+        dispatch({ type: 'LOGOUT' })
+        navigate('/')
       })
-      .catch((error) => {
+      .catch(error => {
         // An error happened.
       })
   }
@@ -53,29 +52,28 @@ export default function Home () {
             placeholder='Search Items ...'
             onChange={e => setSearch(e.target.value)}
           />
-
         </InputGroup>
-        <HStack mr="1%">
-          <Text fontSize="xl" fontWeight="500" mr="4%">
+        <HStack mr='1%'>
+          <Text fontSize='xl' fontWeight='500' mr='4%'>
             {currentUser?.email}
           </Text>
           <Button
-            colorScheme="blue"
-            size="lg"
-            mt="2%"
-            mr="5%"
+            colorScheme='blue'
+            size='lg'
+            mt='2%'
+            mr='5%'
             onClick={handleLogout}
           >
             Logout
           </Button>
         </HStack>
       </Flex>
-      <div className="home">
+      <div className='home'>
         <Filter />
-        <Map data={fakeData} />
+        <Map data={fakeData} search={search} />
         <ResultsBar data={fakeData} search={search} />
         <CreateModal />
       </div>
     </div>
-  );
+  )
 }
