7b4fe2de9f9040975a56c212f76c291376f9d68d
NwinNwin <ndnguyen2003@gmail.com>
2023-05-26 22:28:22 -0700

    added map

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,30 @@
-export default function Home(){
-    return(
-        <div>
-            home
-        </div>
-    )
+import React from "react";
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+
+export default function Home() {
+  function hi() {
+    console.log("hi");
+  }
+  return (
+    <div>
+      <MapContainer
+        style={{ height: "100vh", width: "100wh" }}
+        center={[33.6461, -117.8427]}
+        zoom={17}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[33.6461, -117.8427]} eventHandlers={{ click: hi }}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
