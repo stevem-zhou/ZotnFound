4417f86824150797f93933057abb538062775b13
NwinNwin <ndnguyen2003@gmail.com>
2023-05-26 22:20:27 -0700

    fixed provider bug

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Button,
   Checkbox,
@@ -8,33 +9,33 @@ import {
   Input,
   Link,
   Stack,
-  Image
-} from '@chakra-ui/react'
+  Image,
+} from "@chakra-ui/react";
 
-export default function Login () {
+export default function Login() {
   return (
-    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-      <Flex p={8} flex={1} align={'center'} justify={'center'}>
-        <Stack spacing={4} w={'full'} maxW={'md'}>
-          <Heading fontSize={'2xl'}>Sign in to your account</Heading>
-          <FormControl id='email'>
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <Heading fontSize={"2xl"}>Sign in to your account</Heading>
+          <FormControl id="email">
             <FormLabel>Email address</FormLabel>
-            <Input type='email' />
+            <Input type="email" />
           </FormControl>
-          <FormControl id='password'>
+          <FormControl id="password">
             <FormLabel>Password</FormLabel>
-            <Input type='password' />
+            <Input type="password" />
           </FormControl>
           <Stack spacing={6}>
             <Stack
-              direction={{ base: 'column', sm: 'row' }}
-              align={'start'}
-              justify={'space-between'}
+              direction={{ base: "column", sm: "row" }}
+              align={"start"}
+              justify={"space-between"}
             >
               <Checkbox>Remember me</Checkbox>
-              <Link color={'blue.500'}>Forgot password?</Link>
+              <Link color={"blue.500"}>Forgot password?</Link>
             </Stack>
-            <Button colorScheme={'blue'} variant={'solid'}>
+            <Button colorScheme={"blue"} variant={"solid"}>
               Sign in
             </Button>
           </Stack>
@@ -42,13 +43,13 @@ export default function Login () {
       </Flex>
       <Flex flex={1}>
         <Image
-          alt={'Login Image'}
-          objectFit={'cover'}
+          alt={"Login Image"}
+          objectFit={"cover"}
           src={
-            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+            "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
           }
         />
       </Flex>
     </Stack>
-  )
+  );
 }
