bafaa9197ce69ca39e74834f39ed2df7c248609c
Steven Zhou <stevenz9@uci.edu>
2023-05-26 21:54:05 -0700

    created routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg'
+import { Route, Routes } from "react-router-dom";
 import './App.css'
+import Login from "./components/Login/Login";
 
 function App () {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/cards" element={}/>
+      </Routes>
     </div>
   )
 }
