319f7e41455699675eb40836ee12984f66a2c2a7
Katy Huang <katyhuang360166@gmail.com>
2023-05-26 21:31:22 -0700

    test

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,5 @@
-import React from "react";
+import React from 'react'
 
-export default function Map(){
-    return(
-        <div>
-            
-        </div>
-    )
+export default function Map () {
+  return <div>hi</div>
 }
---
+++ src/components/ResultCard/ResultCard.jsx
@@ -0,0 +1,2 @@
+import * as React from 'react'
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
