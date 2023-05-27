ff963a8197719dc8d1cdc7957a1e7ba592f9043e
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2023-05-27 11:59:14 -0700

    merge master

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './components/Login/Login'
-import Home from './components/Home/Home'
-import ResultCard from './components/ResultCard/ResultCard'
-import CreateModal from './components/CreateModal/CreateModal'
-import { ChakraProvider } from '@chakra-ui/react'
-import 'leaflet/dist/leaflet.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import ResultCard from "./components/ResultCard/ResultCard";
+import CreateModal from "./components/CreateModal/CreateModal";
+import InfoModal from "./components/InfoModal/InfoModal";
 
-function App () {
+import { ChakraProvider } from "@chakra-ui/react";
+import "leaflet/dist/leaflet.css";
+function App() {
   return (
     <ChakraProvider>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/cards' element={<ResultCard />} />
-          ;<Route path='/modals' element={<CreateModal />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cards" element={<ResultCard />} />
+          <Route path="/modals" element={<CreateModal />} />
+          <Route path= "/info" element = {<InfoModal/>}/>
         </Routes>
       </div>
     </ChakraProvider>
