7425de29bcd995864cb54d691b6773a56079f049
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2023-05-27 14:38:42 -0700

    fixed styling of infomodal and linked to results on home

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -11,49 +11,59 @@ import {
   Button,
   Flex,
   Tag,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { InfoIcon } from "@chakra-ui/icons";
+import InfoModal from "../InfoModal/InfoModal.jsx"
 
 export default function ResultCard({ props }) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   console.log(props);
   return (
-    <Card maxW="sm" align={"center"} mb="10px">
-      <CardBody>
-        <Image src={props.imageUrl} alt="Car key" borderRadius="lg" />
-        <Stack mt="6" spacing="3">
+    <>
+      <Card maxW="sm" align={"center"} mb="10px">
+        <CardBody>
+          <Image src={props.imageUrl} alt="Car key" borderRadius="lg" />
+          <Stack mt="6" spacing="3">
+            <Flex justifyContent={"space-between"}>
+              <Text color="blue.600" fontSize="md" fontWeight="bold">
+                {props.name}
+              </Text>
+              <Text color="blue.600" fontSize="sm">
+                {props.uploadDate}
+              </Text>
+            </Flex>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
           <Flex justifyContent={"space-between"}>
-            <Text color="blue.600" fontSize="md" fontWeight="bold">
-              {props.name}
-            </Text>
-            <Text color="blue.600" fontSize="sm">
-              {props.uploadDate}
-            </Text>
+            <Button
+              variant="ghost"
+              colorScheme="blue"
+              leftIcon={<InfoIcon />}
+              size="md"
+              w="20"
+              onClick={onOpen}
+            >
+
+              View
+            </Button>
+            {props.isLost ? (
+              <Button colorScheme="red" ml="20%" size="md" w="40">
+                Lost
+              </Button>
+            ) : (
+              <Button colorScheme="green" ml="20%" size="md" w="40">
+                Found
+              </Button>
+            )}
           </Flex>
-        </Stack>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <Flex justifyContent={"space-between"}>
-          <Button
-            variant="ghost"
-            colorScheme="blue"
-            leftIcon={<InfoIcon />}
-            size="md"
-            w="20"
-          >
-            View
-          </Button>
-          {props.isLost ? (
-            <Button colorScheme="red" ml="20%" size="md" w="40">
-              Lost
-            </Button>
-          ) : (
-            <Button colorScheme="green" ml="20%" size="md" w="40">
-              Found
-            </Button>
-          )}
-        </Flex>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+      <InfoModal onOpen={onOpen} onClose={onClose} isOpen={isOpen} />
+    </>
   );
 }
