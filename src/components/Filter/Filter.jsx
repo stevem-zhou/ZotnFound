6da17606a4dc9129113462c39406397ac0447263
Steven Zhou <stevenz9@uci.edu>
2023-05-27 00:00:03 -0700

    working on filter form

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,19 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import "./Filter.css";
+
 export default function Filter() {
-  return <div>filtereiskflms</div>;
+  return (
+    <div className="filter--bar">
+      <FormControl>
+        <FormLabel>Lost</FormLabel>
+        <Input type="email" />
+        
+      </FormControl>
+    </div>
+  );
 }
